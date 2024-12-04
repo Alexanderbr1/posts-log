10d145b6af8a0acc2a7634d8869d7e0f8459ab04
Брянцев Александр <bryancev.a2@wildberries.work>
2024-12-04 23:09:19 +0300

    feat: add logging

--- internal/repository/logs.go
+++ internal/repository/logs.go
@@ -4,7 +4,9 @@ import (
 	"context"
 	"github.com/Alexanderbr1/posts-log/internal/config"
 	"github.com/Alexanderbr1/posts-log/pkg/domain"
+	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
+	"log"
 )
 
 type LogsRepository struct {
@@ -20,7 +22,24 @@ func NewLogsRepository(cfg *config.Config, db *mongo.Database) *LogsRepository {
 }
 
 func (r *LogsRepository) Insert(ctx context.Context, item domain.LogItem) error {
-	_, err := r.db.Collection(r.cfg.DB.Collection).InsertOne(ctx, item)
+	// Вставляем документ
+	insertResult, err := r.db.Collection(r.cfg.DB.Collection).InsertOne(ctx, item)
+	if err != nil {
+		return err
+	}
 
-	return err
+	// Получаем идентификатор вставленного документа
+	insertedID := insertResult.InsertedID
+
+	// Ищем вставленный документ по идентификатору
+	var insertedItem domain.LogItem
+	err = r.db.Collection(r.cfg.DB.Collection).FindOne(ctx, bson.M{"_id": insertedID}).Decode(&insertedItem)
+	if err != nil {
+		return err
+	}
+
+	// Выводим документ в лог
+	log.Printf("Вставленный документ: %+v\n", insertedItem)
+
+	return nil
 }
